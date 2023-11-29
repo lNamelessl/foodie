60ec1c8ab86666a007dd23caa8bc3304a490ac7f
lNameless <abdulraheemkasim2003@gmail.com>
2023-11-29 19:21:51 +0100

    new commit

--- app/routers/order.py
+++ app/routers/order.py
@@ -40,47 +40,46 @@ def order_main_dish(user_order:schemas.Order, db: Session = Depends(get_db),curr
     db.add(new_order)
     db.commit()
     total = total_cost(db=db,current_user=current_user)
-    return f"message:" "You've successfully placed your order,"  f" [subtotal: ₦{total['total']}]"
+    return f"Message:" " You've successfully placed your order,"  f" [subtotal: ₦{total['total']}]"
 
 # Creates a new order, stores it in the db and returns a response
 @router.post("/side",status_code=status.HTTP_201_CREATED,)
 def order_side_dish(user_order:schemas.Order, db: Session = Depends(get_db),current_user: int = Depends(oauth2.get_current_user)):
-    order = db.query(models.SideDish).filter(models.SideDish.id == order.id).first()
-    if not user_order:
+    order = db.query(models.SideDish).filter(models.SideDish.id == user_order.id).first()
+    if not order:
         raise HTTPException(status.HTTP_404_NOT_FOUND, detail="meal doesn't exist")
+    price = order.price
     new_order = models.Orders(owner_id=current_user.id,order_id=order.id,food=order.side_dish,price=price)
     db.add(new_order)
     db.commit()
     total = total_cost(db=db,current_user=current_user)
-    return f"message:" "You've successfully placed your order,"  f" (subtotal: ₦{total['total']}"
-    
+    return f"Message: You've successfully placed your order,"  f" [subtotal: ₦{total['total']}]"
+
+# # Creates a new order, stores it in the db and returns a response 
 @router.post("/desert",status_code=status.HTTP_201_CREATED,)
 def order_desert(order:schemas.Order, db: Session = Depends(get_db),current_user: int = Depends(oauth2.get_current_user)):
-    global total
     order = db.query(models.Desert).filter(models.Desert.id == order.id).first()
-    price = order.price
     if not order:
         raise HTTPException(status.HTTP_404_NOT_FOUND, detail="meal doesn't exist")
+    price = order.price
     new_order = models.Orders(owner_id=current_user.id,order_id=order.id,food=order.desert,price=price)
-    total_costs = total_cost(price)
+    total= total_cost(db=db,current_user=current_user)
     db.add(new_order)
     db.commit()
-    return f"message:" "You've successfully placed your order,"  f" (subtotal: ₦{total})"
+    return f"Message: You've successfully placed your order,"  f" [subtotal: ₦{total['total']}]"
     
-
-@router.post("/drinks",status_code=status.HTTP_201_CREATED,)
+# Creates a new order, stores it in the db and returns a response
+@router.post("/drinks",status_code=status.HTTP_201_CREATED)
 def order_drinks(order:schemas.Order, db: Session = Depends(get_db),current_user: int = Depends(oauth2.get_current_user)):
-    global total
     order = db.query(models.Drinks).filter(models.Drinks.id == order.id).first()
-    price = order.price
     if not order:
         raise HTTPException(status.HTTP_404_NOT_FOUND, detail="meal doesn't exist")
+    price = order.price
     new_order = models.Orders(owner_id=current_user.id,order_id=order.id,food=order.drinks,price=price)
-    total_costs = total_cost()
+    total = total_cost(db=db,current_user=current_user)
     db.add(new_order)
     db.commit()
-    return f"message:" "You've successfully placed your order,"  f" (subtotal: ₦{total})"
-
+    return f"Message: You've successfully placed your order,"  f" [subtotal: ₦{total['total']}]"
 
 @router.delete("/delete_order",)
 def delete_order(order: schemas.Order, db: Session = Depends(get_db),current_user: int = Depends(oauth2.get_current_user)):
